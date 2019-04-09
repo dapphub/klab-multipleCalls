e27cd4d03485db2743bc0cb8dff52ec482209116
Lev Livnev <lev@liv.nev.org.uk>
2019-04-09 11:07:36 +0100

    add shell.nix

---
+++ shell.nix
@@ -0,0 +1,19 @@
+with import <nixpkgs> {};
+stdenv.mkDerivation {
+  name = "multipleCalls";
+  buildInputs = [
+    flex
+    getopt
+    git
+    gnumake
+    jq
+    nodejs
+    openjdk8
+    parallel
+    zip
+  ];
+  shellHook = ''
+    export PATH=$KLAB_PATH/node_modules/.bin/:$KLAB_PATH/bin:$PATH
+    export KLAB_EVMS_PATH=$KLAB_PATH/evm-semantics
+  '';
+}
